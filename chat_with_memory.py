c9ddf7bd0f586cb90e7757a1089525138daabefe
bharathkreddy <reddybharath.k@gmail.com>
2023-11-12 21:20:26 +0000

    message history persistance added to chatwithmemory

--- chat_with_memory.py
+++ chat_with_memory.py
@@ -1,9 +1,9 @@
 import os
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import HumanMessagePromptTemplate, SystemMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate, SystemMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import LLMChain
-
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
 load_dotenv()  # take environment variables from .env.
 
 chat_model = ChatOpenAI(
@@ -11,10 +11,18 @@ chat_model = ChatOpenAI(
 )
 
 
+memory = ConversationBufferMemory(
+    chat_memory=FileChatMessageHistory("messages.json"),  # saves all the memory to a file, langchain allows dbs too.
+    memory_key="messages",
+    return_messages=True  # for conv.Buffer Memory to return actual messages and not strings
+)
+
+
 chat_prompt = ChatPromptTemplate(
-    input_variables=["user_input"],
+    input_variables=["user_input", "messages"],  # Add "messages" to input variables.
     messages=[
         SystemMessagePromptTemplate.from_template("You are a helpful but sassy AI assistant, who helps people."),
+        MessagesPlaceholder(variable_name="messages"),  # Placeholder for all memory messages.
         HumanMessagePromptTemplate.from_template("{user_input}")
     ]
 )
@@ -22,6 +30,7 @@ chat_prompt = ChatPromptTemplate(
 chat_chain = LLMChain(
     llm=chat_model,
     prompt=chat_prompt,
+    memory=memory,
     output_key="chat_response"  # Gives output with a key called code instead of usual "text" key
 )
 
@@ -32,4 +41,4 @@ while user_input != 'q':
     result = chat_chain({
             "user_input": user_input,
     })
-    print(result)
+    print(result['chat_response'])
